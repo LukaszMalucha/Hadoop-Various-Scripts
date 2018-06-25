de6c30d27abbdd1117424df5423e8ee2b45edd4b
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-06-25 08:41:52 +0000

    mrjob

---
+++ mapreduce.py
@@ -0,0 +1,24 @@
+## Map-Reducer function example for movie ratings count
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+            reducer=self.reducer_count_ratings)
+            ]
+
+
+    def mapper_get_ratings(self, _,line):                                       ## three params - self, possible key reducer, each input line of data
+        (userID, movieID, rating, timestamp) = line.split('\t')                 ## split with tab character
+        yield rating, 1                                                         ## key-value pair (rating and 1 for a count)
+        
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)                                                  ## summing-up '1' values from mapping function
+
+
+if __name__ == '__main__':
+    RatingsBreakdown.run()
